5f52098b0af1fc8fccc965a81501515112afd0de
viktor <vitek230290@gmail.com>
2020-02-13 01:11:18 +0300

    Updates

---
+++ precache-manifest.4875b5c1c76e0403e417b49de81333b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00637a31239ff49c99fb40742d6e865",
+    "url": "/songbird/index.html"
+  },
+  {
+    "revision": "ada67d5969c0692a2294",
+    "url": "/songbird/static/css/main.a30a8922.chunk.css"
+  },
+  {
+    "revision": "be93dfb2d53b53a1a30b",
+    "url": "/songbird/static/js/2.647995fa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/songbird/static/js/2.647995fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada67d5969c0692a2294",
+    "url": "/songbird/static/js/main.b919e99f.chunk.js"
+  },
+  {
+    "revision": "44d422b7846635519d65",
+    "url": "/songbird/static/js/runtime-main.a131254e.js"
+  }
+]);
